c9650822de3237ad18e7b79aadecdd46f8213557
Ricardo <rica14062004@gmail.com>
2023-11-25 17:00:21 -0300

    se agrega informacion del Nro.Piso, Nro.Asiento y tipo de bus a pasaje

--- TiendaPasajesBus/src/main/java/org/Logica/Pasaje.java
+++ TiendaPasajesBus/src/main/java/org/Logica/Pasaje.java
@@ -8,14 +8,17 @@ public class Pasaje {
     private String destino;
     private String nombreCliente;
     private String rutCliente;
+    private String tipoBus;
     private int nroAsiento;
     private int pisoAsiento;
-    public Pasaje(Cliente cliente, Bus bus){
+    public Pasaje(Cliente cliente, Bus bus, int nroAsiento){
         fechaSalida = bus.getRecorrido().getFechaSalida();
         origen = bus.getRecorrido().getOrigen();
         destino = bus.getRecorrido().getDestino();
         nombreCliente = cliente.getNombre();
         rutCliente = cliente.getRut();
+        tipoBus = bus.getTipoBus();
+        this.nroAsiento = nroAsiento;
         if(bus.pisos.getNroAsientosPiso1()-nroAsiento>=0){
             pisoAsiento = 1;
         }
@@ -24,10 +27,17 @@ public class Pasaje {
         }
     }
     public String emitirPasaje(){
+        String piso;
+        if(pisoAsiento==1){
+            piso = "Primer piso";
+        }else{
+            piso = "Segundo piso";
+        }
         String pasaje = "--------------------------------------------\n" +
                         "Fecha de salida: "+fechaSalida.toString()+"\n" +
                         "Trayecto: "+origen+" - "+destino+"\n" +
-                        "Numero de asiento: "+"  |  "+pisoAsiento+"\n"+
+                        "Clase de bus: "+tipoBus+"\n"+
+                        "Numero de asiento: "+nroAsiento+"  |  Piso: "+piso+"\n"+
                         "--------------------------------------------\n" +
                         "Nombre: "+nombreCliente+"  |  Rut: "+rutCliente+"\n" +
                         "--------------------------------------------";
