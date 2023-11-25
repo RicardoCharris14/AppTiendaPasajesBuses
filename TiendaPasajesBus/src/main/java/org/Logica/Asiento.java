4f6db8f199611492cc5a4c08a2ed3174637d819f
Ricardo <rica14062004@gmail.com>
2023-11-25 00:00:58 -0300

    implementacion de las propiedades y metodos de la clase Recorrido

--- TiendaPasajesBus/src/main/java/org/Logica/Asiento.java
+++ TiendaPasajesBus/src/main/java/org/Logica/Asiento.java
@@ -2,7 +2,15 @@ package org.Logica;
 
 public abstract class Asiento {
     protected int numAsiento;
+    protected Boolean disponible;
     public Asiento(int numAsiento){
         this.numAsiento = numAsiento;
+        this.disponible = true;
+    }
+    public int getNumAsiento(){
+        return numAsiento;
+    }
+    public void setDisponible(Boolean disponible){
+        this.disponible = disponible;
     }
 }
--- TiendaPasajesBus/src/main/java/org/Logica/Recorrido.java
+++ TiendaPasajesBus/src/main/java/org/Logica/Recorrido.java
@@ -1,4 +1,27 @@
 package org.Logica;
 
+import java.util.Date;
+
 public class Recorrido {
+    private Date fechaSalida;
+    private String origen;
+    private String destino;
+    public Recorrido(String origen, String destino, Date fechaSalida){
+        this.fechaSalida = fechaSalida;
+        this.origen = origen;
+        this.destino = destino;
+    }
+    public String getDatosRecorrido(){
+        return "Fecha de salida: "+fechaSalida.toString()+"\nTrayecto: "+origen+" - "+destino+"\n";
+    }
+    public String getOrigen(){
+        return origen;
+    }
+    public String getDestino(){
+        return destino;
+    }
+    public Date getFechaSalida(){
+        return fechaSalida;
+    }
+
 }
